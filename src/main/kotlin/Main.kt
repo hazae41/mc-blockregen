055d124f4f742d075a960554d95ded3a6bbee724
hazae41 <hazae41@gmail.com>
2019-06-01 19:32:26 +0200

    Working build

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,26 @@
-package hazae41.minecraft.blockregen.factions
+package hazae41.minecraft.blockregen.griefprevention
 
-import com.massivecraft.factions.entity.BoardColl
-import com.massivecraft.massivecore.ps.PS
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.ConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
-import hazae41.minecraft.kotlin.lowerCase
-import org.bukkit.ChatColor.stripColor
-import org.bukkit.ChatColor.translateAlternateColorCodes
+import me.ryanhamshire.GriefPrevention.GriefPrevention
 import org.bukkit.block.Block
 
 object Config: ConfigFile("config"){
     val enabled by boolean("enabled")
-    val type by string("type")
-    val list by stringList("list")
+    val restoreAdmins by boolean("restore-admin-claims")
+    val restorePlayers by boolean("restore-player-claims")
 }
 
 fun addController(){
     controllers += fun(block: Block) = true.also{
         if(!Config.enabled) return true
-        val list = Config.list.map { it.lowerCase }
-        fun colorless(str: String) = stripColor(translateAlternateColorCodes('&', str))
-        val faction = BoardColl.get().getFactionAt(PS.valueOf(block)).name.lowerCase.let(::colorless)
-        when(Config.type){
-            "whitelist" -> if(faction !in list) return false
-            "blacklist" -> if(faction in list) return false
+        val dataStore = GriefPrevention.instance.dataStore
+        val claim = dataStore.getClaimAt(block.location, false, null) ?: return@also
+        when(claim.isAdminClaim){
+            true -> if(!Config.restoreAdmins) return false
+            false -> if(!Config.restorePlayers) return false
         }
     }
 }
