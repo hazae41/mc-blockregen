a11b6a1101152279d47160e6874fda6c528147d6
hazae41 <hazae41@gmail.com>
2019-06-03 00:06:32 +0200

    Updated to 2.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,12 @@ package hazae41.minecraft.blockregen.worldguard
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.ConfigFile
+import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
-object Config: ConfigFile("config"){
+object Config: PluginConfigFile("config"){
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
