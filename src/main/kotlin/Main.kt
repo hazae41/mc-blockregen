26c5006e0a90010cce7553424ac78198e0f0af56
hazae41 <hazae41@gmail.com>
2019-06-03 00:43:52 +0200

    Added Civs support

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
-package hazae41.minecraft.blockregen.factions
+package hazae41.minecraft.blockregen.civs
 
-import com.massivecraft.factions.entity.BoardColl
-import com.massivecraft.massivecore.ps.PS
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
@@ -10,6 +8,7 @@ import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.ChatColor.stripColor
 import org.bukkit.ChatColor.translateAlternateColorCodes
 import org.bukkit.block.Block
+import org.redcastlemedia.multitallented.civs.towns.TownManager
 
 object Config: PluginConfigFile("config"){
     val enabled by boolean("enabled")
@@ -22,10 +21,10 @@ fun addController(){
         if(!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         fun colorless(str: String) = stripColor(translateAlternateColorCodes('&', str))
-        val faction = BoardColl.get().getFactionAt(PS.valueOf(block)).name.lowerCase.let(::colorless)
+        val town = TownManager.getInstance().getTownAt(block.location).name.lowerCase.let(::colorless)
         when(Config.type){
-            "whitelist" -> if(faction !in list) return false
-            "blacklist" -> if(faction in list) return false
+            "whitelist" -> if(town !in list) return false
+            "blacklist" -> if(town in list) return false
         }
     }
 }
