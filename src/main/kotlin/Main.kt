10bf089f3c2604d1c28ac9211a3f2ed43e1c4e40
hazae41 <hazae41@gmail.com>
2019-06-03 00:06:32 +0200

    Updated to 2.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,12 @@ package hazae41.minecraft.blockregen.factions
 import com.bekvon.bukkit.residence.Residence
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.ConfigFile
+import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
-object Config: ConfigFile("config"){
+object Config: PluginConfigFile("config"){
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
