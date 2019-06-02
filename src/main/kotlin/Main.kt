64068032f1da103d8e73ceda4f5b2fad750efe12
hazae41 <hazae41@gmail.com>
2019-06-03 00:06:32 +0200

    Updated to 2.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,13 @@ package hazae41.minecraft.blockregen.lands
 
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.ConfigFile
+import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
 import hazae41.minecraft.kotlin.lowerCase
 import me.angeschossen.lands.api.landsaddons.LandsAddon
 import org.bukkit.block.Block
 
-object Config: ConfigFile("config"){
+object Config: PluginConfigFile("config"){
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
