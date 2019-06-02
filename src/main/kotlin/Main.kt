e165d5da8f6c0a6d43b547bbbe153e61171b41df
hazae41 <hazae41@gmail.com>
2019-06-03 00:06:32 +0200

    Updated to 2.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,14 @@ import com.massivecraft.factions.Board
 import com.massivecraft.factions.FLocation
 import hazae41.minecraft.blockregen.controllers
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.ConfigFile
+import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.ChatColor.stripColor
 import org.bukkit.ChatColor.translateAlternateColorCodes
 import org.bukkit.block.Block
 
-object Config: ConfigFile("config"){
+object Config: PluginConfigFile("config"){
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
