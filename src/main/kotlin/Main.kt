abe6a27a3a43d3bb953e1a380f401abc8ca00f70
hazae41 <hazae41@gmail.com>
2019-09-07 01:38:45 +0200

    Use new API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package hazae41.minecraft.blockregen.residence
 
 import com.bekvon.bukkit.residence.Residence
-import hazae41.minecraft.blockregen.controllers
+import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
@@ -14,8 +14,8 @@ object Config: PluginConfigFile("config"){
     val list by stringList("list")
 }
 
-fun addController(){
-    controllers += fun(block: Block) = true.also{
+fun addFilter() {
+    filters += fun(block: Block) = true.also {
         if(!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         val residence = Residence.getInstance()?.residenceManager?.getByLoc(block.location)
@@ -30,6 +30,6 @@ fun addController(){
 class Plugin: BukkitPlugin(){
     override fun onEnable() {
         init(Config)
-        addController()
+        addFilter()
     }
 }
