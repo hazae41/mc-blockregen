9f9d5ddd768071863054a2d95d87a8af99e626e5
hazae41 <hazae41@gmail.com>
2019-09-07 01:38:45 +0200

    Use new API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ package hazae41.minecraft.blockregen.factions
 
 import com.massivecraft.factions.entity.BoardColl
 import com.massivecraft.massivecore.ps.PS
-import hazae41.minecraft.blockregen.controllers
+import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
@@ -11,14 +11,14 @@ import org.bukkit.ChatColor.stripColor
 import org.bukkit.ChatColor.translateAlternateColorCodes
 import org.bukkit.block.Block
 
-object Config: PluginConfigFile("config"){
+object Config : PluginConfigFile("config") {
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
 }
 
-fun addController(){
-    controllers += fun(block: Block) = true.also{
+fun addFilter() {
+    filters += fun(block: Block) = true.also {
         if(!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         fun colorless(str: String) = stripColor(translateAlternateColorCodes('&', str))
@@ -30,9 +30,9 @@ fun addController(){
     }
 }
 
-class Plugin: BukkitPlugin(){
+class Plugin : BukkitPlugin() {
     override fun onEnable() {
         init(Config)
-        addController()
+        addFilter()
     }
 }
