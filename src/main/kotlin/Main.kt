bd53f1b7d87e68a21f99a88751ff857854f3d1a5
hazae41 <hazae41@gmail.com>
2019-09-07 01:38:45 +0200

    Use new API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,36 @@
 package hazae41.minecraft.blockregen.worldguard
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
-import hazae41.minecraft.blockregen.controllers
+import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
 import hazae41.minecraft.kotlin.bukkit.init
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
-object Config: PluginConfigFile("config"){
+object Config : PluginConfigFile("config") {
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
 }
 
-fun addController(){
-    controllers += fun(block: Block) = true.also{
-        if(!Config.enabled) return true
+fun addFilter() {
+    filters += fun(block: Block) = true.also {
+        if (!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         val regions = WorldGuardPlugin.inst().run {
             regionContainer.get(block.world)!!.getApplicableRegions(block.location).map { it.id }
         }
-        when(Config.type){
-            "whitelist" -> if(list.intersect(regions).isEmpty()) return false
-            "blacklist" -> if(list.intersect(regions).any()) return false
+        when (Config.type) {
+            "whitelist" -> if (list.intersect(regions).isEmpty()) return false
+            "blacklist" -> if (list.intersect(regions).any()) return false
         }
     }
 }
 
-class Plugin: BukkitPlugin(){
+class Plugin : BukkitPlugin() {
     override fun onEnable() {
         init(Config)
-        addController()
+        addFilter()
     }
 }
