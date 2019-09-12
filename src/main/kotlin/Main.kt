4b8e6423b0fd2928f58997c2a4ed916884ca639b
hazae41 <hazae41@gmail.com>
2019-09-12 22:37:12 +0200

    Use BlockRegen config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,15 @@ package hazae41.minecraft.blockregen.factions
 
 import com.massivecraft.factions.entity.BoardColl
 import com.massivecraft.massivecore.ps.PS
+import hazae41.minecraft.blockregen.Config.Filters
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
-import hazae41.minecraft.kotlin.bukkit.init
+import hazae41.minecraft.kotlin.bukkit.ConfigSection
+import hazae41.minecraft.kotlin.bukkit.info
+import hazae41.minecraft.kotlin.bukkit.severe
 import hazae41.minecraft.kotlin.lowerCase
-import org.bukkit.ChatColor.stripColor
-import org.bukkit.ChatColor.translateAlternateColorCodes
+import net.md_5.bungee.api.ChatColor.stripColor
+import net.md_5.bungee.api.ChatColor.translateAlternateColorCodes
 import org.bukkit.block.Block
 
 object Config : PluginConfigFile("config") {
@@ -19,13 +21,13 @@ object Config : PluginConfigFile("config") {
 
 fun addFilter() {
     filters += fun(block: Block) = true.also {
-        if(!Config.enabled) return true
+        if (!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         fun colorless(str: String) = stripColor(translateAlternateColorCodes('&', str))
         val faction = BoardColl.get().getFactionAt(PS.valueOf(block)).name.lowerCase.let(::colorless)
-        when(Config.type){
-            "whitelist" -> if(faction !in list) return false
-            "blacklist" -> if(faction in list) return false
+        when (Config.type) {
+            "whitelist" -> if (faction !in list) return false
+            "blacklist" -> if (faction in list) return false
         }
     }
 }
