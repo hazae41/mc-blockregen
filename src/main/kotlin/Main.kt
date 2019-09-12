2bb357832ccc7ef54320e7eb8af2bbc235a9fb0e
hazae41 <hazae41@gmail.com>
2019-09-12 22:20:14 +0200

    Use BlockRegen config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
 package hazae41.minecraft.blockregen.worldguard
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import hazae41.minecraft.blockregen.Config.Filters
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
-import hazae41.minecraft.kotlin.bukkit.init
+import hazae41.minecraft.kotlin.bukkit.ConfigSection
+import hazae41.minecraft.kotlin.bukkit.info
+import hazae41.minecraft.kotlin.bukkit.severe
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
-object Config : PluginConfigFile("config") {
+object Config : ConfigSection(Filters, "worldguard") {
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
@@ -30,7 +32,9 @@ fun addFilter() {
 
 class Plugin : BukkitPlugin() {
     override fun onEnable() {
-        init(Config)
         addFilter()
+        info("Added filter")
+        if (!dataFolder.exists()) return
+        severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
     }
 }
