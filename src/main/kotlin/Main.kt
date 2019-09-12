e8f9ab7239837fa934d0f106726ca3d251efba65
hazae41 <hazae41@gmail.com>
2019-09-12 22:23:38 +0200

    Use BlockRegen config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,12 @@ package hazae41.minecraft.blockregen.worldguard
 import com.sk89q.worldedit.bukkit.BukkitWorld
 import com.sk89q.worldedit.math.BlockVector3
 import com.sk89q.worldguard.WorldGuard
+import hazae41.minecraft.blockregen.Config.Filters
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
-import hazae41.minecraft.kotlin.bukkit.init
+import hazae41.minecraft.kotlin.bukkit.ConfigSection
+import hazae41.minecraft.kotlin.bukkit.info
+import hazae41.minecraft.kotlin.bukkit.severe
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
@@ -18,16 +20,16 @@ object Config : ConfigSection(Filters, "worldguard") {
 
 fun addFilter() {
     filters += fun(block: Block) = true.also {
-        if(!Config.enabled) return true
+        if (!Config.enabled) return true
         val list = Config.list.map { it.lowerCase }
         val regions = WorldGuard.getInstance().platform.run {
             val world = BukkitWorld(block.world)
             val vector = BlockVector3.at(block.x, block.y, block.z)
             regionContainer.get(world)!!.getApplicableRegions(vector).map { it.id }
         }
-        when(Config.type){
-            "whitelist" -> if(list.intersect(regions).isEmpty()) return false
-            "blacklist" -> if(list.intersect(regions).any()) return false
+        when (Config.type) {
+            "whitelist" -> if (list.intersect(regions).isEmpty()) return false
+            "blacklist" -> if (list.intersect(regions).any()) return false
         }
     }
 }
