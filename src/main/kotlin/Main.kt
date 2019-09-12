847a4432f231e3c0db095d5f24234cec4058acd7
hazae41 <hazae41@gmail.com>
2019-09-12 22:20:14 +0200

    Use BlockRegen config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,15 @@
 package hazae41.minecraft.blockregen.griefprevention
 
+import hazae41.minecraft.blockregen.Config.Filters
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
-import hazae41.minecraft.kotlin.bukkit.PluginConfigFile
-import hazae41.minecraft.kotlin.bukkit.init
+import hazae41.minecraft.kotlin.bukkit.ConfigSection
+import hazae41.minecraft.kotlin.bukkit.info
+import hazae41.minecraft.kotlin.bukkit.severe
 import me.ryanhamshire.GriefPrevention.GriefPrevention
 import org.bukkit.block.Block
 
-object Config: PluginConfigFile("config"){
+object Config : ConfigSection(Filters, "griefprevention") {
     val enabled by boolean("enabled")
     val restoreAdmins by boolean("restore-admin-claims")
     val restorePlayers by boolean("restore-player-claims")
@@ -25,9 +27,11 @@ fun addFilter() {
     }
 }
 
-class Plugin: BukkitPlugin(){
+class Plugin : BukkitPlugin() {
     override fun onEnable() {
-        init(Config)
         addFilter()
+        info("Added filter")
+        if (!dataFolder.exists()) return
+        severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
     }
 }
