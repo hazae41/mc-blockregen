7346a3fa68b7280e53266cb63dfb6de79cbb0f32
hazae41 <hazae41@gmail.com>
2019-09-14 13:04:20 +0200

    Multiple tasks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package hazae41.minecraft.blockregen.residence
 
 import com.bekvon.bukkit.residence.Residence
-import hazae41.minecraft.blockregen.Config.Filters
+import hazae41.minecraft.blockregen.Config.Tasks.Task
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.ConfigSection
@@ -10,19 +10,20 @@ import hazae41.minecraft.kotlin.bukkit.severe
 import hazae41.minecraft.kotlin.lowerCase
 import org.bukkit.block.Block
 
-object Config : ConfigSection(Filters, "residence") {
+class Filter(task: Task) : ConfigSection(task.Filters(), "residence") {
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
 }
 
 fun addFilter() {
-    filters += fun(block: Block) = true.also {
-        if(!Config.enabled) return true
-        val list = Config.list.map { it.lowerCase }
+    filters += fun Task.(block: Block) = true.also {
+        val filter = Filter(this)
+        if (!filter.enabled) return true
+        val list = filter.list.map { it.lowerCase }
         val residence = Residence.getInstance()?.residenceManager?.getByLoc(block.location)
         val name = residence?.name?.lowerCase ?: return true
-        when(Config.type){
+        when(filter.type){
             "whitelist" -> if(name !in list) return false
             "blacklist" -> if(name in list) return false
         }
@@ -31,9 +32,9 @@ fun addFilter() {
 
 class Plugin : BukkitPlugin() {
     override fun onEnable() {
+        if (dataFolder.exists())
+            severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
         addFilter()
         info("Added filter")
-        if (!dataFolder.exists()) return
-        severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
     }
 }
