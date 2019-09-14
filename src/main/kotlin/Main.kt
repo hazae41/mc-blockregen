61b77fb6587de383c9b730bdfeaa4496408e6d40
hazae41 <hazae41@gmail.com>
2019-09-14 13:04:20 +0200

    Multiple tasks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package hazae41.minecraft.blockregen.lands
 
-import hazae41.minecraft.blockregen.Config.Filters
+import hazae41.minecraft.blockregen.Config.Tasks.Task
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.ConfigSection
@@ -10,24 +10,25 @@ import hazae41.minecraft.kotlin.lowerCase
 import me.angeschossen.lands.api.landsaddons.LandsAddon
 import org.bukkit.block.Block
 
-object Config : ConfigSection(Filters, "lands") {
+class Filter(task: Task) : ConfigSection(task.Filters(), "lands") {
     val enabled by boolean("enabled")
     val type by string("type")
     val list by stringList("list")
 }
 
 fun Plugin.addFilter() = also { plugin ->
-    filters += fun(block: Block) = true.also {
-        if(!Config.enabled) return true
-        val list = Config.list.map { it.lowerCase }
+    filters += fun Task.(block: Block) = true.also {
+        val filter = Filter(this)
+        if (!filter.enabled) return true
+        val list = filter.list.map { it.lowerCase }
         LandsAddon(plugin, true).apply {
             val key = initialize()
             val land = getLandChunk(block.location)?.land ?: return true
             val name = land.name.lowerCase
             disable(key)
-            when(Config.type){
-                "whitelist" -> if(name !in list) return false
-                "blacklist" -> if(name in list) return false
+            when (filter.type) {
+                "whitelist" -> if (name !in list) return false
+                "blacklist" -> if (name in list) return false
             }
         }
     }
@@ -35,9 +36,9 @@ fun Plugin.addFilter() = also { plugin ->
 
 class Plugin : BukkitPlugin() {
     override fun onEnable() {
+        if (dataFolder.exists())
+            severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
         addFilter()
         info("Added filter")
-        if (!dataFolder.exists()) return
-        severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
     }
 }
