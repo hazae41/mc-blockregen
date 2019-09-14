ce921d1d784b5582ca577adc62fc5fccfc255963
hazae41 <hazae41@gmail.com>
2019-09-14 13:04:20 +0200

    Multiple tasks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package hazae41.minecraft.blockregen.griefprevention
 
-import hazae41.minecraft.blockregen.Config.Filters
+import hazae41.minecraft.blockregen.Config.Tasks.Task
 import hazae41.minecraft.blockregen.filters
 import hazae41.minecraft.kotlin.bukkit.BukkitPlugin
 import hazae41.minecraft.kotlin.bukkit.ConfigSection
@@ -9,29 +9,30 @@ import hazae41.minecraft.kotlin.bukkit.severe
 import me.ryanhamshire.GriefPrevention.GriefPrevention
 import org.bukkit.block.Block
 
-object Config : ConfigSection(Filters, "griefprevention") {
+class Filter(task: Task) : ConfigSection(task.Filters(), "griefprevention") {
     val enabled by boolean("enabled")
     val restoreAdmins by boolean("restore-admin-claims")
     val restorePlayers by boolean("restore-player-claims")
 }
 
 fun addFilter() {
-    filters += fun(block: Block) = true.also {
-        if(!Config.enabled) return true
+    filters += fun Task.(block: Block) = true.also {
+        val filter = Filter(this)
+        if (!filter.enabled) return true
         val dataStore = GriefPrevention.instance.dataStore
         val claim = dataStore.getClaimAt(block.location, false, null) ?: return@also
         when(claim.isAdminClaim){
-            true -> if(!Config.restoreAdmins) return false
-            false -> if(!Config.restorePlayers) return false
+            true -> if(!filter.restoreAdmins) return false
+            false -> if(!filter.restorePlayers) return false
         }
     }
 }
 
 class Plugin : BukkitPlugin() {
     override fun onEnable() {
+        if (dataFolder.exists())
+            severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
         addFilter()
         info("Added filter")
-        if (!dataFolder.exists()) return
-        severe("Please put your filter in BlockRegen config and remove ${dataFolder.name} folder")
     }
 }
